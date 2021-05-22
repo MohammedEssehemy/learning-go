f2008a4e17940ba9e19e94fd24deb8aa44df74e4
Mohammed Essehemy <mohammedessehemy@gmail.com>
2021-05-22 13:44:13 +0400

    refactor: separate modules

--- hello/go.mod
+++ hello/go.mod
@@ -2,4 +2,6 @@ module example.com/hello
 
 go 1.16
 
-require rsc.io/quote v1.5.2
+require example.com/greetings v0.0.0-00010101000000-000000000000
+
+replace example.com/greetings => ../greetings
--- hello/hello.go
+++ hello/hello.go
@@ -2,10 +2,37 @@ package main
 
 import (
 	"fmt"
+	"log"
 
-	"rsc.io/quote"
+	"example.com/greetings"
 )
 
 func main() {
-	fmt.Println(quote.Go())
+	// Set properties of the predefined Logger, including
+	// the log entry prefix and a flag to disable printing
+	// the time, source file, and line number.
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+	// Get a greeting message and print it.
+	message, err := greetings.Hello("Essehemy")
+	// If an error was returned, print it to the console and
+	// exit the program.
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// If no error was returned, print the returned message
+	// to the console.
+	fmt.Println(message)
+
+	// A slice of names.
+	names := []string{"Gladys", "Samantha", "Darrin"}
+	// Request greeting messages for the names.
+	messages, err := greetings.Hellos(names)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// If no error was returned, print the returned map of
+	// messages to the console.
+	fmt.Println(messages)
 }
